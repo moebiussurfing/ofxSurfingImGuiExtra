3e41f3a4da21101180e2cc8403ae05e720a378af
moebiusSurfing <moebiussurfing@gmail.com>
2023-01-07 05:41:16 -0300

    adding new notifier but requires C++17

--- 0 Examples WIP/07_Notifier2/src/ofApp.cpp
+++ 0 Examples WIP/07_Notifier2/src/ofApp.cpp
@@ -3,11 +3,6 @@
 //--------------------------------------------------------------
 void ofApp::setup()
 {
-	//notifier.setup("assets/fonts/telegrama_render.otf", 13);
-	//notifier.setup("assets/fonts/overpass-mono-bold.otf", 10, 30);
-
-	notifier.setMessagesLifeTime(4000);
-
 	ui.setup();
 }
 
@@ -20,14 +15,11 @@ void ofApp::draw()
 		{
 			ui.Add(ui.bAutoResize);
 
-			notification_manager.Draw();
 
 			ui.EndWindow();
 		}
 	}
 	ui.End();
-
-	notifier.draw();
 }
 
 //--------------------------------------------------------------
@@ -37,43 +29,41 @@ void ofApp::keyPressed(int key)
 		bGui = !bGui;
 	}
 	
-	// Theme
-	if (key == '1') {
-		ofxImGuiSurfing::ImGui_ThemeMoebiusSurfingV2();
-	}
-	if (key == '2') {
-		ofxImGuiSurfing::ImGui_Xemu();
-	}
-	if (key == '3') {
-		ofxImGuiSurfing::ImGui_ThemeYave();
-	}
+	//// Theme
+	//if (key == '1') {
+	//	ofxImGuiSurfing::ImGui_ThemeMoebiusSurfingV2();
+	//}
+	//if (key == '2') {
+	//	ofxImGuiSurfing::ImGui_Xemu();
+	//}
+	//if (key == '3') {
+	//	ofxImGuiSurfing::ImGui_ThemeYave();
+	//}
 
-	// Queue a notitification
-	if (key == ' ') 
-	{
-		stringstream ss;
-		ss << textAtribute[rand() % 5] << textObject[rand() % 4] << textVerb[rand() % 6] << textSubject[rand() % 5];
-		string str(ss.str());
-		notification_manager.QueueNotification(str.c_str());
-	}
+	//// Queue a notitification
+	//if (key == ' ') 
+	//{
+	//	stringstream ss;
+	//	ss << textAtribute[rand() % 5] << textObject[rand() % 4] << textVerb[rand() % 6] << textSubject[rand() % 5];
+	//	string str(ss.str());
+	//	notification_manager.QueueNotification(str.c_str());
+	//}
 
-	// Queue an error
-	if (key == OF_KEY_BACKSPACE) 
-	{
-		stringstream ss2;
-		ss2 << "We wanted to have " << 2 << " more pints, but it was past " << 11 << " o'clock." << endl;
-		string str2(ss2.str());
-		notification_manager.QueueError(str2.c_str());
-	}
+	//// Queue an error
+	//if (key == OF_KEY_BACKSPACE) 
+	//{
+	//	stringstream ss2;
+	//	ss2 << "We wanted to have " << 2 << " more pints, but it was past " << 11 << " o'clock." << endl;
+	//	string str2(ss2.str());
+	//	notification_manager.QueueError(str2.c_str());
+	//}
 
-	// Queue a notitification
-	if (key == OF_KEY_RETURN) 
-	{
-		stringstream ss;
-		ss << textAtribute[rand() % 5] << textObject[rand() % 4] << textVerb[rand() % 6] << textSubject[rand() % 5];
-		string str(ss.str());
-		notifier.addNotification(str);
-	}
-
-	
+	//// Queue a notitification
+	//if (key == OF_KEY_RETURN) 
+	//{
+	//	stringstream ss;
+	//	ss << textAtribute[rand() % 5] << textObject[rand() % 4] << textVerb[rand() % 6] << textSubject[rand() % 5];
+	//	string str(ss.str());
+	//	notifier.addNotification(str);
+	//}
 }
--- 0 Examples WIP/07_Notifier2/src/ofApp.h
+++ 0 Examples WIP/07_Notifier2/src/ofApp.h
@@ -4,10 +4,6 @@
 
 #include "ofxSurfingImGui.h"
 
-#include "notifications.hh"
-
-#include "ofxNotify.h"
-
 class ofApp : public ofBaseApp {
 
 public:
@@ -15,44 +11,7 @@ public:
 	void draw();
 	void keyPressed(int key);
 
-
 	ofParameter<bool> bGui{ "Show Gui", true };
 
 	ofxSurfingGui ui;
-
-	NotificationManager notification_manager;
-	ofxNotify notifier;
-
-
-	string textAtribute[5] = {
-		"Brilliant",
-		"Curious",
-		"Amazing",
-		"Sublime",
-		"Ironic",
-	};
-
-	string textObject[4] = {
-		" disco inferno",
-		" camp fire entanglement",
-		" wedding promise",
-		" man"
-	};
-
-	string textVerb[6] = {
-		" bites",
-		" strikes",
-		" lost on",
-		" snatches",
-		" teases",
-		" invites"
-	};
-
-	string textSubject[5] = {
-		" pope",
-		" government",
-		" irate schoolmaster",
-		" late enthusiast",
-		" dog"
-	};
 };
