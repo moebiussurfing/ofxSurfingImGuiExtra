f29db7b906e627c09029c2fc4b7e69ab5851d04b
moebiusSurfing <moebiussurfing@gmail.com>
2023-02-07 18:51:12 -0300

    adding new blueprint

---
+++ 6_Nodes/6_5_1_ImNodes_thedmd-Blueprints2/src/ofApp.cpp
@@ -0,0 +1,70 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+	ofSetFrameRate(60);
+	//ofSetWindowPosition(-1920, 25);
+
+	//ui.setImGuiAutodraw(true);
+	ui.setup();
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+}
+
+//--------------------------------------------------------------
+void ofApp::draw()
+{
+	ui.Begin();
+	{
+		ImGuiWindowFlags window_flags = ImGuiWindowFlags_None;
+		if (ui.bAutoResize) window_flags |= ImGuiWindowFlags_AlwaysAutoResize;
+
+		if (bOpen0) {
+			ImGui::Begin("Panels", &bOpen0, window_flags);
+			{
+				ofxImGuiSurfing::ToggleRoundedButton("Show 1", &bOpen1);
+				ofxImGuiSurfing::ToggleRoundedButton("Show 2", &bOpen2);
+				//ImGui::Dummy(ImVec2(0, 10)); // spacing
+			}
+			ImGui::End();
+		}
+
+		//-
+
+		if (bOpen1) drawWidgets();
+	}
+	ui.End();
+}
+
+//--------------------------------------------------------------
+void ofApp::drawWidgets()
+{
+	if (!initialized) {
+		initialized = true;
+
+		// basic-interaction-example
+		//example.Application_Initialize();
+	}
+
+	ImGui::Begin("thedmd /nimgui-node-editor");
+	{
+		// basic-interaction-example
+		//example.Application_Frame();
+	}
+	ImGui::End();
+}
+
+//--------------------------------------------------------------
+void ofApp::exit() {
+	// basic-interaction-example
+	//example.Application_Finalize();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+	if (key = ' ') 
+	{
+	}
+}
